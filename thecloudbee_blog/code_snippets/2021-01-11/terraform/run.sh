56d1594c6dbbb45664e9b66837858a606cbd8c40
amrojsandhu <amrojpals@gmail.com>
2021-01-11 15:32:29 +0530

    #tcb: blog2 added understanding

--- thecloudbee_blog/code_snippets/2021-01-11/terraform/run.sh
+++ thecloudbee_blog/code_snippets/2021-01-11/terraform/run.sh
@@ -1,2 +1,5 @@
 terraform init
-terraform plan -var-file env/lab/configuration.tfvars
+# deploy on lab using the var-file for the lab
+terraform destroy -var-file env/lab/configuration.tfvars
+# now, switch to another flavour by toggling the version
+terraform destroy -var-file env/staging/configuration.tfvars
