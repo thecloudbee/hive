148cd3c6cf3d28d723bd4fcf91ca76a7119a9bad
amrojsandhu <amrojpals@gmail.com>
2021-01-17 23:38:42 +0530

    #tcb: blog 3: elasticsearch analyzers

---
+++ thecloudbee_blog/code_snippets/2021-01-18/elasticsearch/custom-analyzer-query.sh
@@ -0,0 +1,34 @@
+PUT tcb-2021-01-18-special-chars
+{
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "tcb_special_char_analyser": {
+          "type": "custom",
+          "tokenizer": "tcb_special_char_tokenizer",
+					"filter": [
+            "lowercase",
+            "stop"
+          ]
+        }
+      },
+      "tokenizer": {
+        "tcb_special_char_tokenizer": {
+          "type": "char_group",
+          "tokenize_on_chars": [
+            "whitespace",
+            "punctuation"
+          ]
+        }
+      }
+    }
+  },
+  "mappings": {
+    "properties": {
+      "message": {
+        "type": "text",
+        "analyzer": "tcb_special_char_analyser"
+      }
+    }
+  }
+}
---
+++ thecloudbee_blog/code_snippets/2021-01-18/elasticsearch/elasticsearch-index-query.sh
@@ -0,0 +1,4 @@
+POST tcb-2021-01-18-special-chars/_doc/
+{
+  "message": "com.cloud.bee ERROR java.lang.RuntimeException.UnsupportedOperationException The operation get is not supported."
+}
